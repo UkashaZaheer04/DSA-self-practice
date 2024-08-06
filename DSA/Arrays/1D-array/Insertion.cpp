1d2c00c4bfa756f22858a2ed7fdc8186514457a0
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-06 17:25:35 +0500

    Insertion at the beginning

---
+++ DSA/Arrays/1D-array/Insertion.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//  insert element at first index 
+ int n;
+ cout<<"Enter size of array : ";
+ cin>>n;
+ int arr[n];
+ for (int i=0;i<n;i++)
+ {
+    cout<<"Enter elements in array :";
+    cin>>arr[i];
+ }
+  int x;
+  cout<<"Enter the element you want to add in first index :";
+  cin>>x;
+  for (int i=n;i>0;i--)
+  {
+    arr[i]=arr[i-1];
+  }
+   arr[0]= x;
+   n++;
+   cout<<"The updated array is :"<<endl;
+   for (int i=0;i<n;i++)
+   {
+    cout<<arr[i];
+   }
+}
