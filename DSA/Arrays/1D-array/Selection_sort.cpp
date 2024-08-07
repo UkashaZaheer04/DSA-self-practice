fa31985df5e12ed7e8d134f7a21741b662b3ae0d
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 10:53:10 +0500

    ascending or descending order

--- DSA/Arrays/1D-array/Selection_sort.cpp
+++ DSA/Arrays/1D-array/Selection_sort.cpp
@@ -3,13 +3,33 @@ using namespace std;
 int main()
 {
     // in ascending order 
-    int n = 5;
+    /* int n = 5;
     int arr[5] = {4, 6, 2, 8, 5};
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
             if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "Sorted array is " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    } */
+//    in descending order 
+   int n = 5;
+    int arr[5] = {4, 6, 2, 8, 5};
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] < arr[j])
             {
                 int temp = arr[i];
                 arr[i] = arr[j];
