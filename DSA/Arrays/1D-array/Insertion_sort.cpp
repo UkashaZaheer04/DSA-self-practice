9db88919479c1101a250c167a5c95a61cb27053d
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 22:10:29 +0500

    descending order

--- DSA/Arrays/1D-array/Insertion_sort.cpp
+++ DSA/Arrays/1D-array/Insertion_sort.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 int main()
 {
-    // in ascending order 
+    // in ascending order
+    /*  int n = 5;
+     int arr[5] = {4, 6, 2, 8, 5};
+     for (int i = 1; i < n; i++)
+     {
+         int j = i - 1;
+         int key = arr[i];
+
+         while (j >= 0 && arr[j] > key)
+         {
+             arr[j + 1] = arr[j];
+             j = j - 1;
+         }
+         arr[j + 1] = key;
+     }
+     for (int i = 0; i < n; i++)
+     {
+         cout << arr[i] << " ";
+     } */
+    //    In Descending Order
     int n = 5;
     int arr[5] = {4, 6, 2, 8, 5};
     for (int i = 1; i < n; i++)
@@ -10,7 +29,7 @@ int main()
         int j = i - 1;
         int key = arr[i];
 
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 && arr[j] < key)
         {
             arr[j + 1] = arr[j];
             j = j - 1;
