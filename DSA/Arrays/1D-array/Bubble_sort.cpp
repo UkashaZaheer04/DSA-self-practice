a02dbaf98e2b3cb23d0bae95cd840041f0b20ecc
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 10:05:23 +0500

    bubble sort

---
+++ DSA/Arrays/1D-array/Bubble_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // bubble sort
+    int n = 5;
+    int arr[5] = {4, 6, 2, 8, 5};
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
