cd3d4637b99d283b84f49db07c5332df3c7155e0
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 08:20:32 +0500

    deletion of last index

--- DSA/Arrays/1D-array/Deletion.cpp
+++ DSA/Arrays/1D-array/Deletion.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-using namespace std ;
-int mian ()
+#include <iostream>
+using namespace std;
+int main()
 {
-    int n ;
+    // deletion at first index
+    /* int n ;
     cout<<"Enter the size of array :";
     cin>>n;
     int arr[n];
@@ -20,6 +21,23 @@ int mian ()
     for(int i=0;i<n;i++)
     {
         cout<<arr[i];
+    }
+    return 0; */
+    // deletion at last index
+    int n;
+    cout << "Enter the size of array :";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter elements in array :";
+        cin >> arr[i];
+    }
+
+    cout << "After deletion of last  element array is :" << endl;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i];
     }
     return 0;
 }
