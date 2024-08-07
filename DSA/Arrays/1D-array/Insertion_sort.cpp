f244701bd86a30a2c109d3138d9430c9003f80ef
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 22:08:48 +0500

    Insertion sort

---
+++ DSA/Arrays/1D-array/Insertion_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // in ascending order 
+    int n = 5;
+    int arr[5] = {4, 6, 2, 8, 5};
+    for (int i = 1; i < n; i++)
+    {
+        int j = i - 1;
+        int key = arr[i];
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
