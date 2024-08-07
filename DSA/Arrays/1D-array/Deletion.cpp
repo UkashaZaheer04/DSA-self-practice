248b530318df36961ace7238291c1a37d7830328
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-08-07 08:48:14 +0500

    deletion of specific index

--- DSA/Arrays/1D-array/Deletion.cpp
+++ DSA/Arrays/1D-array/Deletion.cpp
@@ -24,17 +24,43 @@ int main()
     }
     return 0; */
     // deletion at last index
+    /*  int n;
+     cout << "Enter the size of array :";
+     cin >> n;
+     int arr[n];
+     for (int i = 0; i < n; i++)
+     {
+         cout << "Enter elements in array :";
+         cin >> arr[i];
+     }
+
+     cout << "After deletion of last  element array is :" << endl;
+     for (int i = 0; i < n - 1; i++)
+     {
+         cout << arr[i];
+     } */
     int n;
-    cout << "Enter the size of array :";
+    cout << "Enter size of array : ";
     cin >> n;
     int arr[n];
+
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter elements in array :";
+        cout << "Enter elements in array ";
         cin >> arr[i];
     }
+    int index;
+    cout << "Enter index in which you want to delete  : ";
+    cin >> index;
+    int x;
+    cout << "Enter the element you want to delete in array : ";
+    cin >> x;
+    for (int i = index; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
 
-    cout << "After deletion of last  element array is :" << endl;
+    cout << "The updated array is : ";
     for (int i = 0; i < n - 1; i++)
     {
         cout << arr[i];
